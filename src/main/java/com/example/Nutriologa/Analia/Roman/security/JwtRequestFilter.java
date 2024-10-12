dd558301efa9c2c3525ab68a804ea37514cfb403
Kevin Ortiz <111302391+kipu1@users.noreply.github.com>
2024-10-11 22:01:41 -0500

    filtro

--- src/main/java/com/example/Nutriologa/Analia/Roman/security/JwtRequestFilter.java
+++ src/main/java/com/example/Nutriologa/Analia/Roman/security/JwtRequestFilter.java
@@ -38,22 +38,30 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);  // Obtiene el token
             username = jwtUtil.extractUsername(jwt); // Extrae el nombre de usuario del token
+            logger.info("JWT Token encontrado: " + jwt);  // Log del token
+        } else {
+            logger.warn("JWT Token no presente o no empieza con 'Bearer '");
         }
 
         // Si el nombre de usuario fue extraído y no hay autenticación previa en el contexto de seguridad
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            logger.info("Autenticando usuario: " + username);
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
 
             if (jwtUtil.validateToken(jwt, userDetails.getUsername())) {
+                logger.info("Token JWT válido para el usuario: " + username);
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            } else {
+                logger.error("Token JWT no válido para el usuario: " + username);
             }
         }
 
         // Continua con el siguiente filtro en la cadena
         chain.doFilter(request, response);
     }
+
 }
